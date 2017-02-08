f3b6d866c90047489f2c4ba0ac4584f1a8798eed
stevegordon <tdtsgordon@gmail.com>
2017-02-07 17:32:56 -0800

    fixed heroku precompile error

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Website
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
